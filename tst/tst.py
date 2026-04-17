d5b01b3536622fff99dd7bbbaec69bce9d0f6769
hicham-darw <185743161+hicham-darw@users.noreply.github.com>
2026-04-17 20:00:45 +0100

    older for testing some cases

--- tst/tst.py
+++ tst/tst.py
@@ -1,7 +1,18 @@
-graph = {
-    'a': "hicham",
-    'b': 'darwin',
-    'c': 'johnwick'
-}
+def searchInsert(nums: list[int], target: int) -> int:
+    left = 0
+    right = len(nums) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        print(nums[left:right + 1])
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+        
+    return left
 
-print(dir(graph.keys()).__iter__)
+
+print(f"res: >>>> {searchInsert([1,3,5,6], 0)}")
+
